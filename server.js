0a5a2a878154f89ae6f6e5048c38e4399d511573
Robi Hidayat <robihidayatrobi@gmail.com>
2019-02-04 17:23:08 +0700

    Fixing errors.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("./startup/config")();
 require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/prod")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => winston.info(`Listening on port ${port}...`));
