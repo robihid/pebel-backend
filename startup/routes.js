42ca7f2e2f362fce8cedd6e79b2de39fb009ac94
Robi Hidayat <robihidayatrobi@gmail.com>
2019-02-04 17:15:00 +0700

    Test

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,9 @@ const penyandangs = require("../routes/penyandangs");
 const error = require("../middlewares/error");
 
 module.exports = app => {
+  app.get("/", (req, res) => {
+    res.send("TEST");
+  });
   app.use("/api/penyandangs", penyandangs);
   app.use(error);
 };
