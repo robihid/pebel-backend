fd3c902e3b85216a3973ccd6398e15579d70319e
Robi Hidayat <robihidayatrobi@gmail.com>
2019-02-04 17:07:32 +0700

    Add production middlewares.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const winston = require('winston');
+const express = require("express");
+const winston = require("winston");
 
 const app = express();
 app.use(express.json());
-require('./startup/config')();
-require('./startup/logging');
-require('./startup/routes')(app);
-require('./startup/db')();
+require("./startup/config")();
+require("./startup/logging");
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/prod")();
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => winston.info(`Listening on port ${port}...`));
