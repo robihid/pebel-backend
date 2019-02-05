20eb75c3b2566a53e0971e944cd1ef47a39b5c2a
Robi Hidayat <robihidayatrobi@gmail.com>
2019-02-05 16:20:03 +0700

    Set database.

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('pebel', 'gwp', 'gwp', {
-  dialect: 'postgres',
-  host: 'localhost'
-});
+const sequelize = new Sequelize(
+  config.get('dbName'),
+  config.get('dbUser'),
+  config.get('dbPassword'),
+  {
+    dialect: 'postgres',
+    host: config.get('dbHost')
+  }
+);
 
 module.exports = sequelize;
